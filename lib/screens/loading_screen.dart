09dd8b22943e100d9860ccce0dfab2c0d8d64a3f
YEAP JO WEE <jowee@YEAPs-MacBook-Pro.local>
2019-12-17 15:09:16 +0800

    Initial Commit

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,12 +9,34 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
+  void initState() {
+    super.initState();
+
+    getWeather();
+  }
+
+  void getLocation() async {
+    Location location = Location();
+    await location.getLocation();
+
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  void getWeather() async {
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+
+    print(response.body);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: RaisedButton(
           onPressed: () {
-            //Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
